b5417aedd33384e9846b959bcdc6e124c410ea0a
Ashish Jacob Benson <160376103+skyxwalker@users.noreply.github.com>
2025-05-19 10:11:47 +0530

    day1

---
+++ 0-introduction.js
@@ -0,0 +1,15 @@
+
+console.log("helo world");
+dt = new Date();
+dt_date = dt.getDate();
+formated_date = dt.getDate();
+console.log(formated_date);
+console.log(dt);
+console.log(dt_date);
+console.log("");
+console.log(typeof (true));
+// console.log(dt.getdate(),"-",dt.getMonth(),"-",dt.getFullYear());
+console.log("todays date is",dt_date);
+console.log("todays date is",dt.getDate());
+console.log(`todays date is ${dt_date}`);
+console.log(`${dt.getDate()}-${dt.getMonth()}-${dt.getFullYear()}`);
